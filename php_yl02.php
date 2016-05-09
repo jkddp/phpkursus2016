abd41704c7fbcc1724fa2ff09ff615105f3ad095
juku@tmk.edu.ee <juku@tmk.edu.ee>
2016-05-09 19:00:44 +0300

    k6ik

---
+++ php_yl02.php
@@ -0,0 +1,53 @@
+﻿<br>--== PHP ülesanded 02 ==--
+<br>PHP kursus 2016
+<br>
+<br>Vastused lisada Githubi andmehoidlasse https://github.com/kasutajanimi/phpkursus2016/ylesanded/yl0201.php järgmine failinimega yl0202.php jne. Lisa koodi sisse kommentaariks failide esimele reaks oma (hüüd)nimi, rühm ja kuupäev.
+<br>Näide: https://github.com/supremecreature/phpkursus2016/tree/master/ylesanded
+<br>
+<br>MASSIIVID
+<br>
+<br>================================
+<br>Ülesanne 0201
+<br>
+<br>Genereeri tsükliga 100 täisarvust koosnev massiiv.
+<br>Väljasta see "print_r" funktsiooniga.
+<br>
+<br>
+<br>
+<br>================================
+<br>Ülesanne 0202
+<br>
+<br>Lisa käsitsi massiivi 10 looma nime kasutades massiivi kohanäitu. Nt. $animals[0]="karu" jne.
+<br>Väljasta see "foreach" tsükkliga.
+<br>
+<br>
+<br>
+<br>================================
+<br>Ülesanne 0203
+<br>
+<br>Lisa käsitsi massiivi kümne näitleja nime kasutades massiivi loomiseks array funktsiooni 
+<br>$stars = array('Clint', 'Bruce'); 
+<br>Väljasta see "for" tsükliga.
+<br>
+<br>
+<br>
+<br>================================
+<br>Ülesanne 0204
+<br>
+<br>Lisa massiivi inimese isikuandmed (nimi, vanus jne.) min 5 elementi.
+<br>Väljasta need mõnes lauses, sellesama inimese kohta.
+<br>Nt. "Kodanik [nimi] on [vanus] aastane"
+<br>
+<br>
+<br>
+<br>================================
+<br>Ülesanne 0205
+<br>
+<br>
+<br>Sisesta käsitsi 3x3 kahemõõtmeline massiiv. Massiivi elementideks sisesta filmide nimed.
+<br>Nt.
+<br>$movies[1][1] = 'Matrix';
+<br>$movies[1][2] = 'Titanic';
+<br>jne.
+<br>Väljasta see massiiv tsüklitega.
+<br>
